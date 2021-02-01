f6591220c914553d581740f02bc587c141fd8457
farhan-nahid <nahidahmed9933@gmail.com>
2021-02-02 00:28:12 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+const number = 0;
+const name = "Ahmed";
+
+// if (number) {
+//   console.log("Condition is true");
+// } else {
+//   console.log("Condition is false");
+// }
+
+if (name) {
+  console.log("Condition is true");
+} else {
+  console.log("Condition is false");
+}
+
+//if number is 00 & the string is empty the default condition is false .
+//if i don't declare the variable value it is undefined & null is negative value.
