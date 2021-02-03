240b2038b67a062596407c3a89d65914fb934660
farhan-nahid <nahidahmed9933@gmail.com>
2021-02-03 21:04:22 +0600

    default

---
+++ akam.js
@@ -0,0 +1,8 @@
+console.log("Starting Hack...");
+console.log("Hacking FBI 0%");
+console.log("Hacking FBI 20%");
+console.log("Hacking FBI 40%");
+console.log("Hacking FBI 60%");
+console.log("Hacking FBI 80%");
+console.log("Hacking FBI 100%");
+console.log("FBI Hacked Successfully");
