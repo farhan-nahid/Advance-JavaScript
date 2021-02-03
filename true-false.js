14a61a07a0c38d55bc1249b3a6773e300a5c1bf9
farhan-nahid <nahidahmed9933@gmail.com>
2021-02-03 21:19:56 +0600

    template

--- true-false.js
+++ true-false.js
@@ -15,11 +15,3 @@ if (name) {
 
 //if number is 00 & the string is empty the default condition is false .
 //if i don't declare the variable value it is undefined & null is negative value.
-console.log("Starting Hack...");
-console.log("Hacking FBI 0%");
-console.log("Hacking FBI 20%");
-console.log("Hacking FBI 40%");
-console.log("Hacking FBI 60%");
-console.log("Hacking FBI 80%");
-console.log("Hacking FBI 100%");
-console.log("FBI Hacked Successfully");
