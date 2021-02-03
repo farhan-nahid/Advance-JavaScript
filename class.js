8f51083f514c97925ad0b0ac5d89a7a525bc1be3
farhan-nahid <nahidahmed9933@gmail.com>
2021-02-04 02:35:28 +0600

    class

--- class.js
+++ class.js
@@ -1,10 +1,10 @@
-class student {
+class Student {
   constructor(stId, stName) {
     this.id = stId;
     this.name = stName;
     this.school = "dhaka collage";
   }
 }
-const student1 = new student(10, "ran");
-const student2 = new student(11, "kan");
+const student1 = new Student(10, "ran");
+const student2 = new Student(11, "kan");
 console.log(student1.name, student2.name);
