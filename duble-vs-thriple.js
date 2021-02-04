0e9f477a52419f274535fb833496b3d1fd47d08d
farhan-nahid <nahidahmed9933@gmail.com>
2021-02-04 14:46:33 +0600

    equal confution

---
+++ duble-vs-thriple.js
@@ -0,0 +1,9 @@
+const first = 0;
+const second = false;
+if (first === second) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
+
+//double equal not check/compare the type but tripoli equal do this
